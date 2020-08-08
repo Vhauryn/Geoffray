a12b8c2b595e1a1da79a8543452bee2ccb194d3c
Eugen Hildt <eugen.hildt@posteo.de>
2020-08-08 13:06:06 +0200

    improved basic_usage example

--- example/basic_usage.dart
+++ example/basic_usage.dart
@@ -2,17 +2,16 @@ import 'package:geoffrey/hooks.dart'
     show useHttpServer, useGet, usePost, useCustom;
 
 void main() {
-  // creates a new HttpServer, binds it to the given
-  // host:port and handles incoming requests
-  useHttpServer('localhost', 8080);
-
-  // setting / as route will throw an Error
-  // this is due to the / route is reserved for 
-  // serving html/files
+  // setting / as a route will throw an Error
+  // this is due to the / route is reserved for serving html/files
   // useGet(
   //     route: '/',
   //     handleRequest: (req, res) => res.write('reserved route!'),
   //     handleGuard: (req, res) => true);
+
+  // creates a new HttpServer, binds it to the given
+  // host:port and handles incoming requests
+  useHttpServer('localhost', 8080);
 
   // creates a new route with GET method
   // or if route exists appends a new method to it
@@ -20,11 +19,11 @@ void main() {
   useGet(
       route: '/home',
       handleRequest: (req, res) => res.write('hallo'),
-      handleGuard: (req, res) => true);
+      handleGuard: (req, res) => true); // handleGuard is optional
 
-  // here we add an additional post method to the route /
+  // here we add an additional post method to the route /home
   // since handleGuard returns false we will get a 422 Unprocessable Entity
-  // thus we won't reach post on route /
+  // thus we won't reach post on route /home
   usePost(
       route: '/home',
       handleRequest: (req, res) => res.write('world'),
@@ -34,6 +33,5 @@ void main() {
   useCustom(
       route: '/home',
       method: 'x-magic-rabbit',
-      handleRequest: (req, res) => res.write('a wild magic rabbit appeared!'),
-      handleGuard: (req, res) => true);
+      handleRequest: (req, res) => res.write('a wild magic rabbit appeared!'));
 }
