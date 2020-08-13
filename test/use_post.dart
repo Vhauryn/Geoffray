22e82e08328ea1d46100c880f002930bc0457093
sch <hotoleanu.ciprian@gmail.com>
2020-08-13 19:25:05 +0300

    Added post

--- test/use_post.dart
+++ test/use_post.dart
@@ -1,17 +1,17 @@
 import 'package:dio/dio.dart';
-import 'package:geoffrey/hooks.dart' show useHttpServer, useGet;
+import 'package:geoffrey/hooks.dart' show useHttpServer, usePost;
 import 'package:test/test.dart';
 
 void main() {
-  test('Test useGet', () async {
+  test('Test usePost', () async {
     try {
       useHttpServer('localhost', 8080);
 
-      useGet(
+      usePost(
           route: '/home',
           handleRequest: (req, res) => res.write('hello'),
           handleGuard: (req, res) => true); // optional
-      Response response = await Dio().get("http://localhost:8080/home");
+      Response response = await Dio().post("http://localhost:8080/home");
       expect(response.toString(), equals('hello'));
     } catch (e) {
       print(e);
