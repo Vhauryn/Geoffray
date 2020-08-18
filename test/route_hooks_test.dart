4504df517d6627b0016537ef0adc70bbb1f25c2e
Eugen Hildt <eugen.hildt@gmail.com>
2020-08-18 17:04:59 +0200

    improved tests

--- test/route_hooks_test.dart
+++ test/route_hooks_test.dart
@@ -2,24 +2,68 @@ import 'dart:io' show HttpServer;
 import 'package:dio/dio.dart';
 import 'package:test/test.dart';
 import 'package:geoffrey/hooks.dart'
-    show useCreateHttpServer, useRequestHandler, useGet, useDelete;
+    show
+        useCreateHttpServer,
+        useRequestHandler,
+        useGet,
+        useDelete,
+        usePatch,
+        usePost,
+        useHead,
+        useCustom;
 
 void main() {
   HttpServer server;
-  setUp(() async => server = await useCreateHttpServer('localhost', 8080));
-  tearDown(() async => await server.close(force: true));
+  Dio dio = new Dio(BaseOptions(
+    baseUrl: "http://localhost:8080",
+    connectTimeout: 5000,
+    receiveTimeout: 3000,
+  ));
 
-  test('Test useGet', () async {
-    useRequestHandler(server);
-    useGet(route: '/home', handleRequest: (req, res) => res.write('home'));
-    Response response = await Dio().get("http://localhost:8080/home");
-    expect(response.toString(), equals('home'));
-  });
+  group('Test HTTP Routes', () {
+    tearDownAll(() async => await server.close(force: true));
+    setUpAll(() async {
+      server = await useCreateHttpServer('localhost', 8080);
+      useRequestHandler(server);
+      useGet(route: '/home', handleRequest: (req, res) => res.write('1'));
+      usePost(route: '/home', handleRequest: (req, res) => res.write('2'));
+      useDelete(route: '/home', handleRequest: (req, res) => res.write('3'));
+      usePatch(route: '/home', handleRequest: (req, res) => res.write('4'));
+      useHead(route: '/home', handleRequest: (req, res) => res.write('5'));
+      useCustom(
+          route: '/home',
+          method: 'x-custom-method',
+          handleRequest: (req, res) => res.write('6'));
+    });
+    test('useGet', () async {
+      Response response = await dio.get("/home");
+      expect(response.toString(), equals('1'));
+    });
 
-  test('Test useDelete', () async {
-    useRequestHandler(server);
-    useDelete(route: '/home', handleRequest: (req, res) => res.write('home'));
-    Response response = await Dio().delete("http://localhost:8080/home");
-    expect(response.toString(), equals('home'));
+    test('usePost', () async {
+      Response response = await dio.post("/home");
+      expect(response.toString(), equals('2'));
+    });
+
+    test('useDelete', () async {
+      Response response = await dio.delete("/home");
+      expect(response.toString(), equals('3'));
+    });
+
+    test('usePatch', () async {
+      Response response = await dio.patch("/home");
+      expect(response.toString(), equals('4'));
+    });
+
+    test('useHead', () async {
+      Response response = await dio.head("/home");
+      expect(response.toString(), equals(''));
+    });
+
+    test('useCustom', () async {
+      dio.options.method = 'x-custom-method';
+      Response response = await dio.request("/home");
+      expect(response.toString(), equals('6'));
+    });
   });
 }
