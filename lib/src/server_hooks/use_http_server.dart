cd545580e8664c9a3b122d04cf8873b08dd0e05b
Eugen Hildt <eugen.hildt@gmail.com>
2020-08-23 12:52:56 +0200

    resolved a bug where Gzip was not working properly

--- lib/src/server_hooks/use_http_server.dart
+++ lib/src/server_hooks/use_http_server.dart
@@ -2,16 +2,20 @@ import 'dart:io';
 import 'use_create_http_server.dart';
 import 'use_request_handler.dart';
 import '../helpers/set_default_response_headers.dart';
+import '../helpers/set_gzip_auto_compress.dart';
 
 /** 
  * + Creates a new HttpServer 
  * + Binds it to the given host:post 
- * + Handles incoming requests*/
+ * + Handles incoming requests
+ * + Uses Gzip by default
+ * */
 Future<HttpServer> useHttpServer(String host, int port,
     {int backlog = 0, bool v60only = false, bool shared = false}) async {
   HttpServer server = await useCreateHttpServer(host, port,
       backlog: backlog, v60only: v60only, shared: shared);
 
+  setGzipAutoCompress(server);
   setDefaultResponseHeaders(server);
   useRequestHandler(server);
 
--- lib/src/server_hooks/use_secure_http_server.dart
+++ lib/src/server_hooks/use_secure_http_server.dart
@@ -2,16 +2,20 @@ import 'dart:io';
 import 'use_create_secure_http_server.dart';
 import 'use_request_handler.dart';
 import '../helpers/set_default_response_headers.dart';
+import '../helpers/set_gzip_auto_compress.dart';
 
 /** 
  * + Creates a new HttpServer 
  * + Secure binds it to the given host:post 
- * + Handles incoming requests*/
+ * + Handles incoming requests
+ * + Uses Gzip by default
+ * */
 Future<HttpServer> useSecureHttpServer(String host, int port, SecurityContext sctx,
     {int backlog = 0, bool v60only = false, bool shared = false}) async {
   HttpServer server = await useCreateSecureHttpServer(host, port, sctx,
       backlog: backlog, v60only: v60only, shared: shared);
 
+  setGzipAutoCompress(server);
   setDefaultResponseHeaders(server);
   useRequestHandler(server);
 
